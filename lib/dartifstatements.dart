ac3b38a124243ad796024cdaa296699e783cbadc
Gikubu <davidndungu8203@gmail.com>
2024-03-04 18:50:38 +0300

    here we check whether the number is less,equal or greater than 10 then gives a response.

--- lib/dartifstatements.dart
+++ lib/dartifstatements.dart
@@ -5,4 +5,16 @@ void main() {
   stdout.write("Enter a number: ");
   // The input is read from the console using stdin.readLineSync().
   String input = stdin.readLineSync()!;
+  //The input is parsed to an integer using int.parse.
+  int number = int.parse(input);
+    //The program checks if the number is greater than,
+    // less than, or equal to 10 and prints the corresponding message.
+    if (number > 10) {
+      print("Your number is greater than 10");
+    } else if (number < 10) {
+      print("Your number is less than 10");
+    } else {
+      print("Your number is equal to 10");
+    }
+  }
 }
